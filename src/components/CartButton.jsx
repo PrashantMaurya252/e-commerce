dce289ac6a687c557a53aaa8e8580754a8a5233a
PrashantMaurya252 <mauryaprashant252@gmail.com>
2023-12-09 07:31:14 +0530

    hover style changing

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CartButton({items,onClick}) {
   return (
-    <button className='bg-gray-200 p-2 border-solid border-red-200 border-2 rounded-xl hover:bg-blue-600 hover:text-gray-200 text-lg font-semibold' onClick={onClick}>CART {items>0 ?<span>{items}</span>:null}</button>
+    <button className='bg-gray-200 p-2 border-solid border-red-200 border-2 rounded-xl hover:bg-blue-600 hover:text-gray-200 text-lg font-semibold' onClick={onClick}>CART {items>0 ?<span className='bg-red-700 p-1 rounded-full text-white'>{items}</span>:null}</button>
   )
 }
 
